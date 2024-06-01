b0ac3f8a3a5cd553a0f2fcdf454be46614c310aa
PROMLEE <donghoon099@naver.com>
2024-06-01 15:31:05 +0900

    Feat: add Notfound and routes

--- Web/src/App.jsx
+++ Web/src/App.jsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
+import {Home, OnBoarding, Timer, Mypage} from './pages';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<div>home</div>} />
-        <Route path="/onboard" element={<div>onBoarding</div>} />
-        <Route path="/timer" element={<div>timer</div>} />
-        <Route path="/mypage" element={<div>mypage</div>} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/onboard" element={<OnBoarding/>} />
+        <Route path="/timer" element={<Timer/>} />
+        <Route path="/mypage" element={<Mypage/>} />
+        <Route path="*" element={<div>404 not found</div>} />
       </Routes>
     </>
   );
---
+++ Web/src/pages/index.js
@@ -0,0 +1,11 @@
+import {Home} from './Home';
+import {Mypage} from './Mypage';
+import { OnBoarding } from './OnBoarding';
+import { Timer } from './Timer';
+
+export {
+  Home,
+  Mypage,
+  OnBoarding,
+  Timer
+};
