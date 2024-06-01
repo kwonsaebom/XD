49ebf4ea8ed0365aa8d182466e5004f1022a2057
PROMLEE <donghoon099@naver.com>
2024-06-01 17:19:15 +0900

    Feat: add Theme

--- Web/src/components/example.js
+++ Web/src/components/example.js
@@ -1,15 +1,20 @@
-import  { exampleApi }  from '../apis/example';
+import { exampleApi } from '../apis/example';
 import React, { useEffect } from 'react';
+import styled from 'styled-components';
 
 export const Example = () => {
-
   useEffect(() => {
     // exampleApi();
   }, []);
 
   return (
     <div>
-      <h1>example</h1>
+      <Ex>example</Ex>
     </div>
   );
 };
+
+const Ex = styled.h1`
+  color: ${(props) => props.theme.colors.success}; // --systeamcolor-success 색 사용(마지막 단어를 소문자로)
+  font: ${(props) => props.theme.fonts.button1_a}; // button1_a 사용
+`;
