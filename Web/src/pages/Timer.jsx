f9f5cd2edb4f8cb2c2c339280a6bb2784665d469
PROMLEE <donghoon099@naver.com>
2024-06-01 15:49:27 +0900

    Feat: zus example

--- Web/src/pages/Timer.jsx
+++ Web/src/pages/Timer.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { useStore } from '../store/test';
 
 export const Timer = () => {
+  const { count, increaseCount, setThree } = useStore();
+  // const {yourState, yourAction} = useYourStore();
+  // yourAction((state) => state.yourState)
   return (
-    <div>Timer</div>
-  )
-}
+    <div>
+      <div>Zustand ! {count}</div>
+      <button onClick={increaseCount}>+1</button>
+      <button onClick={() => setThree(3)}>set3</button>
+    </div>
+  );
+};
